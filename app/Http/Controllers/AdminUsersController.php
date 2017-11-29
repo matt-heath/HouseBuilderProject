ba07dd0e900ad15360acb9c2faa81d2b84953638
Matthew Heath <matthew.heath@hotmail.com>
2017-11-29 21:28:56 +0000

    Users with photos upload working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -49,11 +50,53 @@ class AdminUsersController extends Controller
     {
         //
 
-        User::create($request->all());
+
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        } else{
+
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+
+
+        if($file = $request->file('photo_id')) {
+
+
+            $name = time() . $file->getClientOriginalName();
+
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+
+            $input['photo_id'] = $photo->id;
+
+
+        }
+
+
+        User::create($input);
+
+
+
 
         return redirect('/admin/users');
 
 //        return $request->all();
+
+
+
+
+
+
     }
 
     /**
@@ -77,7 +120,7 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.users.editp');
+        return view('admin.users.edit');
     }
 
     /**
